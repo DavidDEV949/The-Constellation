e35b73d9f8b85db93e785561dbcaf5ff29a95ce0
DavidDEV <daviddev949@gmail.com>
2024-03-05 19:08:35 +0100

    a
    rroz

---
+++ Assets/Scripts/MenuScripts/ExitScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExitScript : MonoBehaviour
+{
+    
+    public void Exit()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+
+}
--- Assets/Scripts/MenuScripts/PlayScript.cs
+++ Assets/Scripts/MenuScripts/PlayScript.cs
@@ -5,40 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
 
     public void Play()
     {
         SceneManager.LoadScene("StarSelection");
-    }
-
-    public void Options(bool toggle)
-    {
-        if (toggle)//david dev, program this. (miler)
-        {
-            //enter
-        }
-        else
-        {
-            //exit
-        }
-    }
-
-    public void Exit()
-    {
-        Application.Quit();
-
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
     }
 
 }
