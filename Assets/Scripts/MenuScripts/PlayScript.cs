9ac70ce4aa60679a1e43e057477e9b195591240f
LeagerStudios <leagerstudios@gmail.com>
2024-03-05 17:42:36 +0100

    Updated UI Design

--- Assets/Scripts/MenuScripts/PlayScript.cs
+++ Assets/Scripts/MenuScripts/PlayScript.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("StarSelection");
+    }
+
+    public void Options(bool toggle)
+    {
+        if (toggle)//david dev, program this. (miler)
+        {
+            //enter
+        }
+        else
+        {
+            //exit
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
 }
