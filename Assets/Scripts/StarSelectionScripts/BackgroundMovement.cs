589594d9e96abb19d26deceaaeec0ae163a9112e
DavidDEV <daviddev949@gmail.com>
2024-03-08 19:32:02 +0100

    j

---
+++ Assets/Scripts/StarSelectionScripts/BackgroundMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMovement : MonoBehaviour
+{
+
+    Transform tr;
+    float SpeedX;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        SpeedX = 1f;
+    }
+
+    void Update()
+    {
+        tr.Translate(-SpeedX * Time.deltaTime, 0, 0);
+    }
+
+}
--- Assets/Scripts/StarSelectionScripts/ChangeSceneScript.cs
+++ Assets/Scripts/StarSelectionScripts/ChangeSceneScript.cs
@@ -1,18 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeSceneScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void StarSelection(GameObject StarToGo)
     {
-        
+        SceneManager.LoadScene(StarToGo.name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
