f4ca3a80907bb3507df4e1b6710bf51c89cae8a8
LeagerStudios <111186389+LeagerStudios@users.noreply.github.com>
2024-03-24 13:25:29 +0100

    Update Player.cs part 2

--- Assets/Scripts/Minigame1/Player.cs
+++ Assets/Scripts/Minigame1/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour {
     public float yV = 0f;
     public float acceleration = 10f;
     public float maxGravity = 10f;
+    public float timer = 0f;
+
+    public bool coinOneUnlocked = false;
+    public bool coinTwoUnlocked = false;
+    public bool coinThreeUnlocked = false;
 
     void Start ()
     {
@@ -33,6 +38,39 @@ public class Player : MonoBehaviour {
 
         transform.eulerAngles = Vector3.forward * ((yV / maxGravity) * 40);
         rb2D.velocity = new Vector2(0, yV);
+        timer += Time.deltaTime;
+
+        if(timer > 15f)
+        {
+          if(coinOneUnlocked)
+          {
+if(timer > 25f)
+        {
+          if(coinTwoUnlocked)
+          {
+            if(timer > 35f)
+        {
+          if(coinThreeUnlocked)
+          {
+
+          }
+          else 
+          {
+ 
+          }
+        }
+          }
+          else 
+          {
+ 
+          }
+        }
+          }
+          else 
+          {
+ 
+          }
+        }
     }
 
     IEnumerator AsteroidLoop()
