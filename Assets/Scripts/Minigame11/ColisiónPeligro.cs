7b6519aaf63c0054cde9872396c55f5aeda3e3ad
Haritz5837 <161754912+Haritz5837@users.noreply.github.com>
2024-06-03 14:56:44 +0200

    ColisiónPeligro_Script

---
+++ Assets/Scripts/Minigame11/ColisiónPeligro.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisiónPeligro : MonoBehaviour
+{
+    // Este método se llama cuando el objeto comienza a colisionar con otro objeto
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica si el objeto con el que se ha colisionado tiene la etiqueta "Peligro"
+        if (collision.gameObject.CompareTag("Peligro"))
+        {
+            // Aquí puedes poner el código que se debe ejecutar al detectar la colisión con el objeto peligroso
+            Debug.Log("¡Colisión con objeto peligroso detectada!");
+        }
+    }
+}
