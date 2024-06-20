0edf64a464cd8c9599401c3c206970ea0066a14e
Haritz5837 <161754912+Haritz5837@users.noreply.github.com>
2024-06-20 17:19:57 +0200

    Some changes

--- Assets/Scripts/Minigame4/OttoM4.cs
+++ Assets/Scripts/Minigame4/OttoM4.cs
@@ -1,18 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
-public class Salto : MonoBehaviour
+public class OttoM4 : MonoBehaviour
 {
     public float jumpForce = 10f; // Fuerza del salto
     private Rigidbody2D rb;
     private bool isGrounded; // Indica si el jugador está tocando el suelo
+    bool HasMuerto = false;
+    private int puntuacion = 0;
+    public Text puntuacionText;
+
+    void PanConChocolate()
+    {
+        puntuacion++;
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("PanConChocolate", 0.2f, 0.2f);
     }
 
     void Update()
     {
+        puntuacionText.text = puntuacion.ToString();
+
         // Si se presiona la tecla espacio y el jugador está en el suelo, saltar
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
@@ -35,5 +49,19 @@ public class Salto : MonoBehaviour
         {
             isGrounded = false; // El jugador deja de tocar el suelo
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Peligro"))
+        {
+            Debug.Log("Te moristeeee M4");
+            Muerto();
+        }
+    }
+
+    void Muerto()
+    {
+        HasMuerto = true;
     }
 }
